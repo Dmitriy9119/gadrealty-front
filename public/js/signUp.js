1f0c9ae3072f731891819dfa31a17d31ef026319
Dmitriy9119 <deserd@ukr.net>
2022-03-26 03:19:39 +0200

    refactor + each input onInput validation

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,19 +1,49 @@
-// Validation SignUp
+// // Validation SignUp
 (function () {
     'use strict';
     window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        // Validation of each input
         const forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        const validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
+        const inputValidation = Array.prototype.filter.call(forms, function (form) {
+            if (form.id !== "mainForm") {
+                const input = document.getElementById(`${form.id}Input`);
+                input.addEventListener('blur', function () {
+                    !input.value && input.id !== "passwordConfirmationInput" && input.classList.add('is-invalid')
+                }, false);
+                input.addEventListener('input', function () {
+                    form.classList.add('was-validated')
+                    if (input.id !== "passwordConfirmationInput") {
+                        if (form.checkValidity()) {
+                            input.classList.remove('is-invalid')
+                            form.classList.add('was-validated')
+                            form.checkValidity() && input.classList.add('is-valid')
+                        } else {
+                            input.classList.remove('is-valid')
+                            form.classList.add('was-validated')
+                            !form.checkValidity() && input.classList.add('is-invalid')
+                        }
+                    }
+                }, false);
+            }
         });
+
+        // Validation of all inputs after submit button click
+        const submitButton = document.getElementById('submitButton');
+        submitButton.addEventListener('click', function (event) {
+            const check = [];
+            const forms = $('.needs-validation');
+            for (let i = 0; i < forms.length; i++) {
+                forms[i].classList.add('was-validated')
+                forms[i].checkValidity() && check.push(true)
+            }
+            if (check.length === forms.length) {
+                console.log('validated successfully') //что-то делаем дальше...
+            } else {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+        }, false);
     }, false);
 })();
-// End Validation SignUp
+// // End Validation SignUp
+
